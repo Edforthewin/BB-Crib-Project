49fad884da5622df335b8515fe3e7196a78b38c8
Edforthewin <edwinramirezgomez14@gmail.com>
2024-11-22 12:10:35 -0500

    phase 0

---
+++ frontend/src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import './index.css';
+// import { Provider } from 'react-redux';
+import configureStore from './store/store';
+
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
