0974505554640ca81b1038d161ec5926e26ecbc0
Edforthewin <edwinramirezgomez14@gmail.com>
2024-12-15 20:41:41 -0500

    form validat

--- frontend/src/components/NewSpot/NewSpot.jsx
+++ frontend/src/components/NewSpot/NewSpot.jsx
@@ -27,6 +27,36 @@ function NewSpotFormModal() {
 
     });
 
+    const formValidate = () => {
+        const errors = {};
+        if(!form.country) errors.country = 'Need that country';
+        if(!form.address) errors.address = 'Street addy is required';
+        if(!form.city) errors.city = 'City is required';
+        if(!form.state) errors.state = 'State is required';
+        if(form.desription.length < 30) errors.desription = 'Description needs 30 or more chars';
+        if(!form.name) errors.name = 'Name is required';
+        if(!form.price) errors.price = 'Price per night is required';
+        if(!form.previewImage) errors.previewImage = 'Preview image is required';
+        return errors;
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const errors = formValidate();
+
+        if(Object.keys(errors).length > 0) {
+            setErrors(errors);
+            return;
+        }
+
+        try {
+            const newSpot = await dispatch(spotActions(form));
+            navigate(`/spots/${newSpot.id}`);
+        } catch(error) {
+            setErrors({ submit: error.message });
+        }
+    };
+
     return (
 
     )
