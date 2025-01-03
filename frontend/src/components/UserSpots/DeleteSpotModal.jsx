4016658df44e5889943ef09e6b96c97e6bb540da
Edforthewin <edwinramirezgomez14@gmail.com>
2025-01-03 18:57:51 -0500

    delete spot modal

---
+++ frontend/src/components/UserSpots/DeleteSpotModal.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import { Modal } from '../..context/Modal';
+import { DeleteSpot } from './DeleteSpot';
+
+export default function DeleteSpotModal({spot}) {
+    const [ showModal, setShowModal ] = useState(false);
+
+    return (
+        <>
+          <button onClick={() => setShowModal(true)} className='button-delete'>Delete</button>
+          {showModal && (
+            <Modal>
+              <DeleteSpot spot={spot} onClose={() => setShowModal(false)} />
+            </Modal>
+          )}
+        </>
+      );
+    }
