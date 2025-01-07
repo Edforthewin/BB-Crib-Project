be067ecf1f9ba2c3435bfd8fa4a387a719f65e10
Edforthewin <edwinramirezgomez14@gmail.com>
2025-01-07 16:05:28 -0500

    index.jsx for filter

---
+++ frontend/src/components/Listing-Spots/SpotHead/Filter/FilterBox.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import * as spotActions from '../../../../store/spots';
+import './FilterBox.css';
+import { useHistory } from 'react-router-dom';
+
+const FilterBox = ({onClose, dispatch}) => {
+
+}
+
+export default FilterBox
