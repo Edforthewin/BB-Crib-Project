a97402e6bf1b42013ae99c8a934ad8200e9b4b5d
Edforthewin <edwinramirezgomez14@gmail.com>
2025-01-07 18:56:12 -0500

    footer comp

---
+++ frontend/src/components/Footer/index.jsx
@@ -0,0 +1,36 @@
+
+import './index.css';
+
+function Footer() {
+    return (
+        <footer>
+            <div className='footer'>
+                <div className='footer-left'>
+                    <div style={{fontWeight: 700}}>
+                        2024 BB-Cribs
+                    </div>
+                    <div className='personal-link'>
+                        <a href='https://github.com/Edforthewin'>Github</a>
+                    </div>
+                    <div>
+                        Destinations
+                    </div>
+                </div>
+                <div></div>
+                <div className='footer-right'>
+                    <div>
+                        English(US)
+                    </div>
+                    <div>
+                        $ USD
+                    </div>
+                    <div>
+                        Support
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
