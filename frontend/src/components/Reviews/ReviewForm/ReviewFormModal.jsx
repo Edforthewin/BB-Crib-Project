1d4658e90a10b86d57e412a57b0f94f8ba0321a4
Edforthewin <edwinramirezgomez14@gmail.com>
2025-01-07 18:22:42 -0500

    reviewForm

---
+++ frontend/src/components/Reviews/ReviewForm/ReviewFormModal.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import { Modal } from '../../../context/Modal';
+import { ReviewForm } from '.'
+
+function ReviewSpotModal({ spotId }) {
+    const [showModal, setShowModal] = useState(false);
+
+    return (
+        <>
+          <button onClick={() => setShowModal(true)} className='button-create'>Add a review</button>
+          {showModal && (
+            <Modal>
+              <ReviewForm spotId={spotId} onClose={() => setShowModal(false)} />
+            </Modal>
+          )}
+        </>
+      );
+}
+
+export default ReviewSpotModal
