05f02075422ad49bec4315844e0569543667bcb3
Edforthewin <edwinramirezgomez14@gmail.com>
2025-01-10 18:17:29 -0500

    loginfomral refactor

--- frontend/src/components/LoginFormModal/LoginFormModal.jsx
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -21,65 +21,65 @@ function LoginFormModal() {
   const handleSubmit = (e) => {
     e.preventDefault();
     setErrors([]);
-    return dispatch(sessionActions.login({ credential, password }))
-      .then(closeModal)
-      .catch(async (res) => {
-        const data = await res.json();
-        if (data && data.errors) {
-          setErrors(data.errors);
-        }
-      });
-  };
+    return dispatch(sessionActions.userLogin({ credential, password }))
+        .then(() => closeModal(false))
+        .catch(async (res) => {
+            const data = await res.json();
+            if (data && data.errors) {
+                setErrors([`username/email or password are incorrect!`]);
+            }
+        })
+}
 
-  return (
-    <div className='login-container'>
-       <div className='login-header'>
-       <h2>Log In</h2>
-       </div>
-      <form onSubmit={handleSubmit} className='login-css'>
-        <div className='login-welcome'>
-          <h3>Welcome to BB Cribs</h3>
-        </div>
+return (
+  <div className='login-container'>
+      <div className='login-header'>
+          <h2>Log in</h2>
+      </div>
+      <form onSubmit={handleSubmit} className='login-form'>
+          <div className='login-welcome'>
+              <h3>Welcome to Hairbnb</h3>
+          </div>
 
-        {errors.length > 0 &&
-          <ul>
-             {errors.length.map(error =>
-               <li key={error}>{error}</li>)}
-          </ul>
-        }
+          {errors.length > 0 &&
+              <ul>
+                  {errors.map(error =>
+                      <li key={error}>{error}</li>)}
+              </ul>
+          }
 
-        <div className='login-info'>
-           <div className='login-name'>
-                <label>
-                   <input
-                    type='text'
-                    value={credential}
-                    onChange={(e) => setCredential(e.target.value)}
-                    required
-                    placeholder='username/email'
-                    className='login-input login-input-email'
-                   />
-                </label>
-           </div>
-           <div className='login-password'>
-              <label>
-                <input
-                    type='password'
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                    placeholder='password'
-                    className='login-input'
-                  />
-              </label>
-           </div>
-        </div>
-        <div className='login-button'>
-            <button className='login-btn' type='submit'>Log in</button>
-        </div>
+          <div className='login-info'>
+              <div className='login-name'>
+                  <label>
+                      <input
+                          type='text'
+                          value={credential}
+                          onChange={(e) => setCredential(e.target.value)}
+                          required
+                          placeholder='username/email'
+                          className='login-input login-input-email'
+                      />
+                  </label>
+              </div>
+              <div className='login-password'>
+                  <label>
+                      <input
+                          type='password'
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                          required
+                          placeholder='password '
+                          className='login-input'
+                      />
+                  </label>
+              </div>
+          </div>
+          <div className='login-button'>
+              <button className='login-button' type='submit'>Log in</button>
+          </div>
       </form>
-    </div>
-  );
+  </div>
+)
 }
 
 export default LoginFormModal;
