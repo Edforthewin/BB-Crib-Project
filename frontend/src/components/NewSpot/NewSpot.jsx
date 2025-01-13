aaf9faeacad32ccc9da13c901a4ff956e6410efb
Edforthewin <edwinramirezgomez14@gmail.com>
2025-01-13 18:17:18 -0500

    newspot refactor

--- frontend/src/components/NewSpot/NewSpot.jsx
+++ frontend/src/components/NewSpot/NewSpot.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import * as spotActions from '../../store/spots';
+import { makeSpot } from '../../store/spots';
+import { uploadSpotImage } from '../../store/spots';
 import './NewSpot.css';
 
 
@@ -9,6 +10,7 @@ import './NewSpot.css';
 function NewSpotFormModal() {
     const navigate = useNavigate();
     const dispatch = useDispatch();
+
     const [name, setName] = useState('');
     const [desription, setDescription] = useState('');
     const [price, setPrice] = useState('');
@@ -16,45 +18,82 @@ function NewSpotFormModal() {
     const [city, setCity] = useState('');
     const [state, setState] = useState('');
     const [country, setCountry] = useState('');
-    const lat = 47.823;
-    const lng = 123;
-    const [url, setUrl] = useState('');
-    const [errors, setErrors ] = useState([]);
-    const preview = true;
-    const sessionUser = useSelector((state) => state.session.user);
+    const [previewImage, setPreviewImage] = useState('');
+    const [lat, setLat] = useState('');
+    const [lng, setLng] = useState('');
+    const [imageUrls, setImageUrls] = useState('');
+    const [errors, setErrors ] = useState({});
 
-    if(!sessionUser) return (
-        <div className='spot-welcome'>
-            <h2>Login to see this page!</h2>
-        </div>
-    )
+
+    const handleImageChange = (index, value) => {
+        const newImageUrls = [...imageUrls];
+        newImageUrls[index] = value;
+        setImageUrls(newImageUrls);
+    }
 
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        setErrors([]);
-        if(url.length > 256) setErrors(['The length of URL must be less than 256 characters'])
 
+        const newErrors = {};
+        if(!country) newErrors.country = "Country is required.";
+        if(!address) newErrors.address = "Street address is required.";
+        if(!city) newErrors.city = "City is required";
+        if(!state) newErrors.state = "State is required";
+        if(desription.length < 30) newErrors.desription = "Description needs 30 or more characters.";
+        if(!name) newErrors.name = "Name is required";
+        if(!price) newErrors.price = "Price per night is required.";
+        if (!lat || lat< -90 || lat > 90) newErrors.lat = "Latitude must be between -90 and 90.";
+        if (!lng || lng < -180 || lng > 180) newErrors.lng = "Longitude must be between -180 and 180.";
+        if (!previewImage) newErrors.previewImage = "Preview image is required.";
 
-    let createdSpot = await dispatch(spotActions.makeSpot({name, desription, price, address, country, city, state, lat, lng, url, preview}))
-        .catch(async res => {
-            const data = await res.json();
-            if (data & data.message) {
-                if (data.errors) {
-                    const errors = Object.values(data.errors);
-                    setErrors(errors);
-                } else {
-                    setErrors(data.message);
-                }
-            }
-        })
+        const imageErrors = imageUrls.map(url => {
+            const isValid = url === '' || /\.(png|jpg|jpeg)$/.test(url);
+            return isValid ? '' : 'Image URL needs to end in png or jpg (or jpeg)';
+        });
 
-        if (createdSpot) {
-            const id = createdSpot.id
-            navigate.push(`/spots/${id}`)
+        if (imageErrors.some(error => error !== '')) {
+            newErrors.imageErrors = imageErrors;
         }
 
-   }
+        if (Object.keys(newErrors).length > 0) {
+            setErrors(newErrors);
+            return;
+        }
+
+        const data = {
+            name,
+            desription,
+            address,
+            city,
+            state,
+            country,
+            price: parseFloat(price),
+            previewImage,
+            lat: parseFloat(lat),
+            lng: parseFloat(lng),
+            images: imageUrls.filter(image => image)
+        };
+
+        try {
+            const result = await dispatch(makeSpot(data));
+
+            if(result && result.id) {
+                await dispatch(uploadSpotImage(result.id, imageUrls, previewImage));
+                alert("Spot successfully created!");
+                navigate(`/spots/${result.id}`);
+            } else {
+                alert("Failed to create spot. Please check your inputs.");
+            }
+        } catch (error) {
+            const errorMessage = error.response?.data?.error || 'An error occurred while creating the spot.';
+            alert(errorMessage);
+            if (error.response?.data?.errors) {
+                setErrors(error.response.data.errors);
+            }
+        }
+   };
+
 
    return (
         <div className='newspot-main'>
@@ -64,12 +103,7 @@ function NewSpotFormModal() {
                     <h2>Host your crib</h2>
             </div>
             <form onSubmit={handleSubmit} className='spot-form'>
-            {errors.length > 0 &&
-            <ul>
-                {errors.map(error =>
-                    <li key={error}>{error}</li>)}
-            </ul>
-        }
+
         <div className='newspot-field'>
             <label>
                 <input
@@ -81,6 +115,7 @@ function NewSpotFormModal() {
                 className='input-fieldSpot'
 
               />
+              {errors.address && <p>{errors.address}</p>}
             </label>
         </div>
         <div className='newspot-field'>
@@ -92,8 +127,8 @@ function NewSpotFormModal() {
                 required
                 placeholder='City'
                 className='input-fieldSpot'
-
               />
+              {errors.city && <p>{errors.city}</p>}
             </label>
         </div>
         <div className='newspot-field'>
@@ -107,6 +142,34 @@ function NewSpotFormModal() {
                 className='input-fieldSpot'
 
               />
+              {errors.state && <p>{errors.state}</p>}
+            </label>
+        </div>
+        <div className='newspot-field'>
+            <label>
+                <input
+                type='text'
+                value={lat}
+                onChange={(e) => setLat(e.target.value)}
+                required
+                placeholder='Latitude'
+                className='input-fieldSpot'
+              />
+              {errors.lat && <p>{errors.lat}</p>}
+            </label>
+        </div>
+        <div className='newspot-field'>
+            <label>
+                <input
+                type='text'
+                value={lng}
+                onChange={(e) => setLng(e.target.value)}
+                required
+                placeholder='Longitude'
+                className='input-fieldSpot'
+
+              />
+              {errors.lng && <p>{errors.lng}</p>}
             </label>
         </div>
         <div className='newspot-field'>
@@ -118,8 +181,8 @@ function NewSpotFormModal() {
                 required
                 placeholder='Country'
                 className='input-fieldSpot'
-
               />
+              {errors.country && <p>{errors.country}</p>}
             </label>
         </div>
         <div className='newspot-field'>
@@ -133,6 +196,7 @@ function NewSpotFormModal() {
                 className='input-fieldSpot'
 
               />
+              {errors.name && <p>{errors.desription}</p>}
             </label>
         </div>
         <div className='newspot-field'>
@@ -145,6 +209,7 @@ function NewSpotFormModal() {
                 className='input-fieldSpot'
 
               />
+              {errors.desription && <p>{errors.desription}</p>}
             </label>
         </div>
         <div className='newspot-field'>
@@ -154,25 +219,40 @@ function NewSpotFormModal() {
                 value={price}
                 onChange={(e) => setPrice(e.target.value)}
                 required
-                placeholder='$Price'
+                placeholder='Price per night (USD)'
                 className='input-fieldSpot'
                 min='1'
-
               />
+              {errors.price && <p>{errors.price}</p>}
             </label>
         </div>
         <div className='newspot-field'>
             <label>
                 <input
-                type='url'
-                value={url}
-                onChange={(e) => setUrl(e.target.value)}
+                type='text'
+                value={previewImage}
+                onChange={(e) => setPreviewImage(e.target.value)}
                 required
-                placeholder='Image Link'
+                placeholder='Preview Image URL'
                 className='input-fieldSpot'
 
               />
+              {errors.previewImage && <p>{errors.previewImage}</p>}
             </label>
+
+            {imageUrls.map((url, index) => (
+                        <div key={index}>
+                            <input
+                                type="text"
+                                value={url}
+                                onChange={(e) => handleImageChange(index, e.target.value)}
+                                placeholder="Image URL"
+                            />
+                            {errors.imageErrors && errors.imageErrors[index] && (
+                                <p>{errors.imageErrors[index]}</p>
+                            )}
+                        </div>
+            ))}
         </div>
         <button type='submit'>Agree & Submit</button>
         </form>
--- frontend/src/components/NewSpot/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from 'react-router-dom';
-
-function NewSpot() {
-    return (
-        <Link to='/spots/new'>Be a crib Host</Link>
-    )
-}
-
-export default NewSpot
