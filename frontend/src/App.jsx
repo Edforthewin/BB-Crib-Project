3fbf6906b8c2a39a01993e5155281cd698f0627d
Edforthewin <edwinramirezgomez14@gmail.com>
2025-01-16 16:43:58 -0500

    app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,12 @@ import { useDispatch } from 'react-redux';
 import { createBrowserRouter, RouterProvider, Outlet} from 'react-router-dom';
 import * as sessionActions from './store/session';
 import Navigation from './components/Navigation/Navigation';
-import NewSpot from './components/NewSpot/NewSpot';
-import Footer from './components/Footer/index';
-import SpotsLayout from './components/LandingPage/SpotsLayout';
-import ManageSpots from './components/ManageSpots/ManageSpots';
-import UpdateSpot from './components/ManageSpots/UpdateSpot';
-import SpotDetails from './components/SpotDetails/SpotDetails';
+// import NewSpot from './components/NewSpot/NewSpot';
+// import Footer from './components/Footer/index';
+// import SpotsLayout from './components/LandingPage/SpotsLayout';
+// import ManageSpots from './components/ManageSpots/ManageSpots';
+// import UpdateSpot from './components/ManageSpots/UpdateSpot';
+// import SpotDetails from './components/SpotDetails/SpotDetails';
 import './index.css'
 
 function Layout() {
@@ -34,26 +34,7 @@ const router = createBrowserRouter([
   {
     element: <Layout />,
     children: [
-      {
-        path: '/',
-        element: <SpotsLayout/> && <Footer/>
-      },
-      {
-        path: '/spots/:spotId',
-        element: <SpotDetails/>
-      },
-      {
-        path: '/spots/new',
-        element: <NewSpot/>
-      },
-      {
-        path: '/spots/current',
-        element: <ManageSpots/>
-      },
-      {
-        path: '/spots/:spotId/edit',
-        element: <UpdateSpot/>
-      }
+
     ]
   }
 ]);
